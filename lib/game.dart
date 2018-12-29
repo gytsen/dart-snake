5b379ae4a7bbaa7c12edc541e4b415f4174cdc51
Joost Heitbrink <joostheitbrink@outlook.com>
2018-12-29 09:34:29 +0100

    Add docs to Game class
    Also make all its members private since we want the user to only
    interface with the Game through the game class.

--- lib/game.dart
+++ lib/game.dart
@@ -16,70 +16,101 @@ class Game {
   static const int FPS_MILLIS = 1000 ~/ FPS;
   static const Duration TIMEOUT = const Duration(milliseconds: FPS_MILLIS);
 
-  Random random;
+  Snake _snake;
 
-  Snake snake;
-  Screen screen;
+  Screen _screen;
 
-  Coordinate apple;
+  Coordinate _apple;
 
-  Timer timer;
+  Random _random;
 
-  VoidCallback gameOverCallback;
+  Timer _timer;
+
+  /// A user supplied callback that gets called on game over
+  /// this function should handle and UI changes needed to present
+  /// the player with a nice game over state.
+  VoidCallback _gameOverCallback;
 
   Game(VoidCallback cb) {
     var output = querySelector('#output');
-    screen = new Screen(
-      output,
-    );
+    _screen = new Screen(output);
 
-    random = new Random();
+    _random = new Random();
 
-    snake = Snake.getDefault();
-    apple = generateApple();
+    _snake = Snake.getDefault();
+    _apple = generateApple();
 
-    gameOverCallback = cb;
+    _gameOverCallback = cb;
 
     document.onKeyPress.listen(handleKeypress);
   }
 
+  /// So this series of functions seems a bit weird, but that has to do with the
+  /// asynchronous nature of game ticks and timing. We don't want to fry our CPU
+  /// by blindly redrawing every time something changes, so we need to use the [window.requestAnimationFrame()]
+  /// function to ensure we only paint on screen redraw.
+  ///
+  /// Next, we have our internal game tick, which in this case also dictates our "FPS".
+  /// Thus, we need a periodic timer to handle that case.
+  /// What we now simply do is when the start function is called, is actually create the periodict timer
+  /// with the animate function as a callback.
+
   void start() {
-    timer = Timer.periodic(TIMEOUT, animate);
+    _timer = Timer.periodic(TIMEOUT, animate);
   }
 
+  /// The animate function simply requests an animation frame,
+  /// and passes the actual [gameTick] function
+  /// which contains all the logic for one game tick.
   void animate(Timer unused) {
     window.requestAnimationFrame(gameTick);
   }
 
+  /// [gameTick] contains all the logic necessary to execute exactly one
+  /// game step. The basic algorithm is as follows:
+  /// 1. update the [Snake]'s direction to its requested direction
+  /// 2. create a new head for the snake, but don't add it yet.
+  /// 3. wrap the head around the screen, if necessary
+  /// 4. if the new head is present in the body, it means we crashed into ourselves,
+  ///    so that's game over. On game over we stop the timer and call the user-supplied
+  ///    game over callback to do bookkeeping/cleanup for the game over screen.
+  /// 5. check if we hit the apple. If we did, preserve our tail on move.
+  /// 6. add the new head to the [Snake].
+  /// 7. redraw the screen with the new information.
   void gameTick(num unused) {
     bool preserveTail = false;
-    screen.clear();
 
-    snake.updateDirection();
+    // for now it's easy to just wipe the screen entirely
+    // but later on we might only erase the coordinates that we
+    // actually don't need anymore
+    _screen.clear();
+
+    _snake.updateDirection();
 
     Coordinate head = newSnakeHead();
 
-    if (snake.body.contains(head)) {
-      timer.cancel();
-      gameOverCallback();
+    head = _screen.wrap(head);
+
+    if (_snake.body.contains(head)) {
+      _timer.cancel();
+      _gameOverCallback();
     }
 
-    if (head == apple) {
-      apple = generateApple();
+    if (head == _apple) {
+      _apple = generateApple();
       preserveTail = true;
     }
 
-    snake.addNewHead(head, preserveTail: preserveTail);
+    _snake.addNewHead(head, preserveTail: preserveTail);
 
-    screen.drawCoordinates(snake.body);
-    screen.drawCoordinate(apple, color: '#F00');
+    _screen.drawCoordinates(_snake.body);
+    _screen.drawCoordinate(_apple, color: '#F00');
   }
 
   Coordinate newSnakeHead() {
-    Coordinate head = snake.head.copy();
+    Coordinate head = _snake.head.copy();
 
-    head += Snake.directionToPoint[snake.direction];
-    head = screen.wrap(head);
+    head += Snake.directionToPoint[_snake.direction];
     return head;
   }
 
@@ -88,8 +119,8 @@ class Game {
 
     do {
       apple = new Coordinate(
-          random.nextInt(screen.width), random.nextInt(screen.height));
-    } while (snake.body.contains(apple));
+          _random.nextInt(_screen.width), _random.nextInt(_screen.height));
+    } while (_snake.body.contains(apple));
 
     return apple;
   }
@@ -104,19 +135,19 @@ class Game {
     switch (wrapped.keyCode) {
       case KeyCode.UP:
       case 119:
-        snake.requestChangeDirection(Direction.up);
+        _snake.requestChangeDirection(Direction.up);
         break;
       case KeyCode.DOWN:
       case 115:
-        snake.requestChangeDirection(Direction.down);
+        _snake.requestChangeDirection(Direction.down);
         break;
       case KeyCode.LEFT:
       case 97:
-        snake.requestChangeDirection(Direction.left);
+        _snake.requestChangeDirection(Direction.left);
         break;
       case KeyCode.RIGHT:
       case 100:
-        snake.requestChangeDirection(Direction.right);
+        _snake.requestChangeDirection(Direction.right);
         break;
       default:
         break;
