ded7b95ef223dc2096140e0eed603e2b1c54dbb3
Joost Heitbrink <joostheitbrink@outlook.com>
2018-12-30 22:29:03 +0100

    add a barebones interface

--- web/main.dart
+++ web/main.dart
@@ -1,7 +1,16 @@
+import 'dart:html';
 import 'package:dart_snake/game.dart';
 
 void main() {
-  var game = new Game(onGameOver);
+  var startButton = document.querySelector('#start');
+  startButton.onClick.listen(onClick);
+}
+
+void onClick(MouseEvent e) {
+  const String level = '''
+  [[4,4],[4,6],[4,8],[6,4],[6,6],[6,8],[8,4],[8,6],[8,8]]
+  ''';
+  var game = new Game.withLevel(onGameOver, level);
   game.start();
 }
 
