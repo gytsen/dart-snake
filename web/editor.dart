271f05593646a6e6c6ef14b4acda0e1e156f16dc
Joost Heitbrink <joostheitbrink@outlook.com>
2019-01-05 02:11:55 +0100

    Add map editor
    Also add a finally working map editor in the mix, which allows you to create custom maps for playing with!

--- web/editor.dart
+++ web/editor.dart
@@ -1,11 +1,14 @@
 import 'dart:html';
+import 'dart:convert';
 
 import 'package:dart_snake/screen.dart';
 import 'package:dart_snake/coordinate.dart';
-import 'package:dart_snake/wall_parser.dart';
+import 'package:dart_snake/game_map.dart';
 
 Screen screen;
-List<Coordinate> map;
+Set<Coordinate> map;
+
+SpanElement wallCount;
 
 Point getCanvasPosition(CanvasElement canvas, MouseEvent e) {
   var rect = canvas.getBoundingClientRect();
@@ -17,20 +20,53 @@ Point getCanvasPosition(CanvasElement canvas, MouseEvent e) {
 void onClick(MouseEvent e) {
   var location = getCanvasPosition(screen.canvas, e);
   var coordinate = screen.getCoordinateFromCanvas(location);
-  map.add(coordinate);
+  if (map.contains(coordinate)) {
+    map.remove(coordinate);
+  } else {
+    map.add(coordinate);
+  }
+
+  screen.clear();
   screen.drawCoordinates(map, color: '#B62');
+  wallCount.text = map.length.toString();
 }
 
-void onSave(MouseEvent e) {
-  var output = WallParser.encode(map);
-  print(output);
+void save(MouseEvent e) {
+  var output = GameMap.encode(screen.width, screen.height, map);
+  download('map.json', output);
+}
+
+/// Little function to immediately download a file
+/// inspired by http://stackoverflow.com/questions/3665115/create-a-file-in-memory-for-user-to-download-not-through-server
+void download(String filename, String content) {
+  Uri encodedContent = Uri.dataFromString(content, encoding: Utf8Codec());
+
+  var element = document.createElement('a')
+    ..setAttribute('href', encodedContent.toString())
+    ..setAttribute('download', filename)
+    ..style.display = 'none';
+
+  document.body.append(element);
+
+  element.click();
+
+  element.remove();
+}
+
+void reset(MouseEvent e) {
+  screen.clear();
+  map.clear();
+  wallCount.text = map.length.toString();
 }
 
 void main() {
-  var output = document.querySelector('#editor');
-  var saveButton = document.querySelector('#save');
-  saveButton.onClick.listen(onSave);
-  screen = new Screen(output);
-  map = new List<Coordinate>();
+  CanvasElement canvas = document.querySelector('#map-canvas');
+  wallCount = document.querySelector('#wall-count');
+  var resetButton = document.querySelector('#reset');
+  var saveButton = document.querySelector('#download-map');
+  saveButton.onClick.listen(save);
+  resetButton.onClick.listen(reset);
+  screen = new Screen(canvas);
+  map = new Set<Coordinate>();
   screen.canvas.onClick.listen(onClick);
 }
